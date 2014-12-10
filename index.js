e1ec6a311123be7c37fdeda41b58ffb4c632d6ec
yukjin <luzhen@sungymobile.com>
2014-12-10 14:51:52 +0800

    添加入口

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * Created by luzhen on 14-12-10.
+ */
+var MessageClient=require('./MessageClient');
+var MESSAGE_ENUM=require('./MessageEnum');//引入消息枚举
+var message=require('./Message');//引入消息模块
+exports.MessageClient=MessageClient;
+exports.MESSAGE_ENUM=MESSAGE_ENUM;
+exports.message=message;
